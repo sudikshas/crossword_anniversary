0edba89787ec42918ae7f144cc1a93d72896c851
Sudiksha <ssarvepalli@berkeley.edu>
2026-07-13 19:27:08 -0400

    Organize crossword assets

---
+++ src/data/memoryPhotos.js
@@ -0,0 +1,9 @@
+export const memoryPhotos = [
+  "/memory_photos/addams_photo.jpg",
+  "/memory_photos/beacon_photo.jpg",
+  "/memory_photos/carnegie_hall_photo.jpg",
+  "/memory_photos/junoon_food.jpg",
+  "/memory_photos/junoon_post.jpg",
+  "/memory_photos/penguin_photo.jpg",
+  "/memory_photos/whale_photo.jpg"
+];
