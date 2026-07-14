75d6c1e58b9a9edb7cae44e862431a43516adf7b
Sudiksha <ssarvepalli@berkeley.edu>
2026-07-14 19:09:09 -0400

    code template

--- src/data/memoryPhotos.js
+++ src/data/memoryPhotos.js
@@ -2,6 +2,7 @@ export const memoryPhotos = [
   "/memory_photos/addams_photo.jpg",
   "/memory_photos/beacon_photo.jpg",
   "/memory_photos/carnegie_hall_photo.jpg",
+  "/memory_photos/hell_kitch_rooftop.jpg",
   "/memory_photos/junoon_food.jpg",
   "/memory_photos/junoon_post.jpg",
   "/memory_photos/penguin_photo.jpg",
