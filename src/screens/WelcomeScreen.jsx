10036bcd80cd9f729fa8596b208971204c41e90c
Sudiksha <ssarvepalli@berkeley.edu>
2026-07-14 23:36:07 -0400

    code template

--- src/screens/WelcomeScreen.jsx
+++ src/screens/WelcomeScreen.jsx
@@ -3,7 +3,7 @@ function WelcomeScreen({ onBegin }) {
     <div className="screen welcome-screen">
       <div className="welcome-card">
         <span className="welcome-ornament" aria-hidden="true" />
-        <h1 className="welcome-card-title">Our 3 Year Anniversary Crossword</h1>
+        <h1 className="welcome-card-title">Ansh and Sudi's 3-Year Anniversary Crossword</h1>
         <p className="welcome-card-message">
           Here is a puzzle I made from memories and special moments between the both of us.
           Have fun solving it and going back down memory lane!
